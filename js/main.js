0f144052b5aa182c7803e29a5dbc77e9c73930f3
lucas garcia <garcialj97@gmail.com>
2022-06-28 20:46:58 -0300

    header color update

--- js/main.js
+++ js/main.js
@@ -1,20 +1,14 @@
+
 const navBar = document.getElementById("nav__bar");
 const buttonClose = document.getElementById("nav__button--close");
 const buttonOpen = document.getElementById("nav__button--open");
 
-const openNav = () => {
-    buttonOpen.onclick = (e) => {
-        e.preventDefault();
-        navBar.style.width = "100%";
-    }
-}
-
-const closeNav = () => {
-    buttonClose.onclick = (e) => {
+buttonClose.addEventListener("click", closeNav = (e) => {
         e.preventDefault();
         navBar.style.width = "0%";
-    }
-}
+});
 
-
-
+buttonOpen.addEventListener("click", openNav = (e) => {
+        e.preventDefault();
+        navBar.style.width = "100%";
+});
